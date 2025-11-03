9a6eb3acb99c16aba29fe6fb75002f297cfa4fc1
Ro Oeurn <ro@nowopen.studio>
2025-11-03 09:53:13 -0800

    new

--- app/components/textBlock.tsx
+++ app/components/textBlock.tsx
@@ -15,7 +15,7 @@ export default function TextBlock({title,footnote,copy,subhead, cta,arrow,top,ri
     <React.Fragment>
         <div className={`w-full grid grid-cols-12 gap-x-9 ${top?"items-start":"items-end"} px-9 `}>
                   <div className={`col-span-full grid grid-cols-2 py-9 border-b   relative ${off?'':'border-t mt-9'}`}>
-                    <Reveal styleSet="uppercase col-span-2"><PortableText value={title} /></Reveal>
+                    <Reveal styleSet="uppercase col-span-2 title"><PortableText value={title} /></Reveal>
                     {arrow?( <div className="scrollArrow absolute  top-9 right-0"><ScrollArrow  className="w-[30px] h-auto" fill={"#000000"} /></div>):('')}
                     {arrowBot?( <div className="scrollArrow absolute  bottom-9 right-0"><ScrollArrow  className="w-[30px] h-auto" fill={"#000000"} /></div>):('')}
                     {cta?(<div className="cta inline-block absolute top-9 right-0 secondary"><p>{cta.label}</p></div>):('')}
