ce9d84e0bfff41afa381983bb2b70e2bf8e57980
Ro Oeurn <ro@nowopen.studio>
2025-11-06 11:12:42 -0800

    new

--- app/components/floorplans.tsx
+++ app/components/floorplans.tsx
@@ -51,15 +51,53 @@ export default function Floorplans({ data, filter, cta }: any) {
     setActive(true)
   }
 
-    const zoomOff=()=>{
-  
-  const curr = document.getElementById('body')
-  
-  if(curr){
-    curr.classList.remove('lightBox')
+  const zoomOff = () => {
+
+    const curr = document.getElementById('body')
+
+    if (curr) {
+      curr.classList.remove('lightBox')
+    }
+    setActive(false)
   }
-  setActive(false)
-}
+
+  const back = () => {
+    if (curr[0] == 0 && curr[1] == 0) {
+      if(0==data.length-1){
+         setCurr([0,data[0].rooms.length-1])
+      }else{
+      setCurr([data.length-1, (data[data.length - 1].rooms.length) - 1])}
+    }
+    else if (curr[1] == 0) {
+      if(0==data.length-1){
+         setCurr([curr[0], data[curr[0]].rooms.length-1])
+      }else{
+      setCurr([curr[0] - 1, (data[curr[0] - 1].rooms.length) - 1])}
+    }
+    else {
+      setCurr([curr[0], curr[1] - 1])
+    }
+     console.log(curr)
+
+  }
+
+  const next = () => {
+   
+    if ((curr[0] == data.length - 1) && (curr[1] == data[curr[0]].rooms.length-1)) {
+      setCurr([0,0])
+    }
+    else if (curr[1] == data[curr[0]].rooms.length - 1) {
+      if(0==data.length-1){
+         setCurr([curr[0], 0])
+      }else{
+      setCurr([curr[0] + 1, 0])}
+    }
+    else {
+      setCurr([curr[0], curr[1] + 1])
+    }
+
+     console.log(curr)
+  }
 
 
 
@@ -77,7 +115,7 @@ export default function Floorplans({ data, filter, cta }: any) {
           <div className="w-full">
 
             {filter ? (
-              <div className="flex flex-wrap justify-center lg:justify-start gap-y-4 gap-x-9 lg:gap-4  mb-9 px-18 lg:px-0  z-1" style={{opacity:active?0:1}}>
+              <div className="flex flex-wrap justify-center lg:justify-start gap-y-4 gap-x-9 lg:gap-4  mb-9 px-18 lg:px-0  z-1" style={{ opacity: active ? 0 : 1 }}>
                 <div onClick={() => toggleFilter(0)} className={`${cat == 0 ? 'active' : ''} cta filter cursor-pointer inline-block`}><p>All</p></div>
                 {data.map((item: any, i: number) => {
                   return (
@@ -110,7 +148,7 @@ export default function Floorplans({ data, filter, cta }: any) {
                               </div>
                               <div className="flex  items center text-gray uppercase">
                                 <div onClick={() => changeImage(i, p)} className="px-4 border-r border-gray hover:text-black"><p className='label'>view</p></div>
-                                <a target="__black" href={`${point.cta && point.cta.file?`${point.cta.file}/${point.cta.og}`:(point.cta?point.cta.url : '/')}`}><div className="px-4 hover:text-black"> <p className='label'>download</p></div></a>
+                                <a target="__black" href={`${point.cta && point.cta.file ? `${point.cta.file}/${point.cta.og}` : (point.cta ? point.cta.url : '/')}`}><div className="px-4 hover:text-black"> <p className='label'>download</p></div></a>
                               </div>
                             </div>
                           )
@@ -118,7 +156,7 @@ export default function Floorplans({ data, filter, cta }: any) {
                       ) : ('')}
                     </div>
 
-                      
+
 
                   )
                 })
@@ -131,16 +169,16 @@ export default function Floorplans({ data, filter, cta }: any) {
           </div>
         </div>
         <div className="col-span-6 px-9 hoverOn hidden lg:block">
-          <div className="w-full mb-9"> {data[curr[0]].rooms[curr[1]].image ? (<SwitchContent credits caption work={data[curr[0]].rooms[curr[1]]} title={'Building Spec'} audio={false} />) : ('')}</div>
+          <div className="w-full mb-9 fadeOn" key={`${curr[0]}-${curr[1]}`}> {data[curr[0]].rooms[curr[1]].image ? (<SwitchContent credits caption work={data[curr[0]].rooms[curr[1]]} title={'Building Spec'} audio={false} />) : ('')}</div>
         </div>
       </div>
 
       <div className={`fixed w-full h-full top-0 left-0 z-100 bg-black ${!active ? "pointer-events-none" : ''}`} style={{ transition: 'opacity .5s ease-in-out', opacity: active ? 1 : 0, }}>
-        
+
         <div className="flex justify-center w-full items-center py-20 px-20 h-full">
-         
-            <SwitchContent work={data[curr[0]].rooms[curr[1]]} title={`amenities`} audio={true} contain />
-       
+
+          <SwitchContent work={data[curr[0]].rooms[curr[1]]} title={`amenities`} audio={true} contain />
+
         </div>
         {/* <div className="w-1/2 h-full z-40 left-0 absolute cursor-w-resize hoverOn" onClick={back}>
                       <div className="w-[32px] h-auto absolute top-1/2 left-0 translate-y-[-50%] onHover">
@@ -150,14 +188,24 @@ export default function Floorplans({ data, filter, cta }: any) {
                 <div className="w-[32px] h-auto absolute top-1/2 right-0 translate-y-[-50%] onHover"><GalleryR className="w-full h-auto" />  </div>
                 
                </div> */}
-            <a href={data[curr[0]].rooms[curr[1]].cta?`${data[curr[0]].rooms[curr[1]].cta.file}/${data[curr[0]].rooms[curr[1]].cta.og}`:'/'} className="absolute z-100 label text-white uppercase top-4 left-4 lg:top-9 lg:left-9 font-semibold">
-              <p>Download</p>
-            </a>
-           <div onClick={()=>zoomOff()} className={`cursor-pointer flex uppercase items-end flex-col justify-between w-[42px] h-[16px] absolute top-9 right-9 z-[100]   `}>
-                <div className="w-full border-b-[2px] border-white h-[1px] singleBar topBar" style={{transform:`rotate(45deg)`,transformOrigin:"25% 30%"}}></div>
-                
-                <div className="w-full border-b-[2px] border-white h-[1px] singleBar botBar" style={{transform:`rotate(-45deg)`,transformOrigin:"25% 30%"}}></div>
-              </div>
+        <a href={data[curr[0]].rooms[curr[1]].cta ? `${data[curr[0]].rooms[curr[1]].cta.file}/${data[curr[0]].rooms[curr[1]].cta.og}` : '/'} className="absolute z-100 label text-white uppercase top-4 left-4 lg:top-9 lg:left-9 font-semibold">
+          <p>Download {data[curr[0]].rooms[curr[1]].name}</p>
+        </a>
+
+        <div className="w-1/2 h-full top-0 z-99 left-0 absolute cursor-w-resize hoverOn" onClick={back}>
+          <div className="w-[32px] h-auto absolute top-1/2 left-4 md:left-9 translate-y-[-50%] onHover">
+            <GalleryL className="w-full h-auto" /></div>
+        </div>
+        <div className="w-1/2 h-full top-0 z-99 left-1/2 absolute cursor-e-resize hoverOn" onClick={next}>
+          <div className="w-[32px] h-auto absolute top-1/2 right-4 md:right-9 translate-y-[-50%] onHover"><GalleryR className="w-full h-auto" />
+          </div>
+
+        </div>
+        <div onClick={() => zoomOff()} className={`cursor-pointer flex uppercase items-end flex-col justify-between w-[42px] h-[16px] absolute top-9 right-9 z-[100]   `}>
+          <div className="w-full border-b-[2px] border-white h-[1px] singleBar topBar" style={{ transform: `rotate(45deg)`, transformOrigin: "25% 30%" }}></div>
+
+          <div className="w-full border-b-[2px] border-white h-[1px] singleBar botBar" style={{ transform: `rotate(-45deg)`, transformOrigin: "25% 30%" }}></div>
+        </div>
       </div>
 
     </React.Fragment>
